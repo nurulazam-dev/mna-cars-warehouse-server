2e732250b1d19970a36b29fe0b1b8cbd3cc58e2f
mnaofficialbd <mna.official92@gmail.com>
2025-06-12 23:18:01 +0600

    user routes setup

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const itemRoutes = require("./routes/itemRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ connectDB();
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("MNA Car Warehouse - api running");
