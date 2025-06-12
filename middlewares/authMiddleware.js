6b279222106a443ed2aa09a06ff50d62d06053da
mnaofficialbd <mna.official92@gmail.com>
2025-06-12 23:29:49 +0600

    server update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ function verifyToken(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Access Denied" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Invalid Token" });
     req.user = decoded;
     next();
