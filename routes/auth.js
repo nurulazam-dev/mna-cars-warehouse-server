4421688817f29ff880dcee841aa1a9acadc3081b
mnaofficialbd <mna.official92@gmail.com>
2025-06-19 01:28:22 +0600

    resetPassword route create

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,13 @@ import {
   register,
   login,
   forgotPassword,
+  resetPassword,
 } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
